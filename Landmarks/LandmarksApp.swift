64e8c931d9e4d530229dde03aefa116be5c5908e
kazuki <kaz.tokyo.tech20@gmail.com>
2021-05-09 13:43:03 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by fujii kazuki on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
